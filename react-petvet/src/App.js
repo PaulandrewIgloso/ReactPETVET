1b769e69569600491c8984ed740654da4ea94e5c
PaulandrewIgloso <iglosopaul310@gmail.com>
2026-07-09 15:55:03 +0800

    shadcn components

---
+++ react-petvet/src/App.js
@@ -0,0 +1,7 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { LoginForm } from './components/layout/auth/LoginForm';
+import './App.css';
+function App() {
+    return _jsx(LoginForm, {});
+}
+export default App;
---
+++ react-petvet/src/components/layout/auth/LoginForm.d.ts
@@ -0,0 +1,1 @@
+export declare function LoginForm(): import("react").JSX.Element;
